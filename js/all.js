bed0562f89b871e85f000f7abcce7ad551def061
Vic <Vic@gmail.com>
2021-01-13 10:20:30 +0800

    更新 渲染空氣數值至 HMTL

--- js/all.js
+++ js/all.js
@@ -4,7 +4,8 @@ const areasOptions = document.querySelector('.js-areas')
 const searchAir = function() {
   const area =  document.querySelector('.js-area')
   const updateTime = document.querySelector('.js-updateTime')
-  const sites = document.querySelector('.js-sites')
+  const siteDetail = document.querySelector('.js-siteDetail')
+  const siteOther = document.querySelector('.js-siteOther')
   let date = []
   let areas = []
   let areasStr = '<option selected>-請選擇地區-</option>'
@@ -46,16 +47,47 @@ const searchAir = function() {
   } 
   this.changeFn = (e) => {
     let sitesAry = []
-    let sitesStr = ''
+    let siteDetailStr = ''
+    let sitesOtherStr = ''
     area.textContent = e.target.value
     date.forEach(item => {
       if ( item.County === e.target.value ) {
         sitesAry.push(item)
       }
     })
+    let siteFirst = sitesAry.shift()
+    siteDetailStr = `
+    <div class="col-7 text-center">
+      <span class="h2 fw-bolder">${siteFirst.SiteName}</span>
+    </div>
+    <div class="col-5 py-4 border-start border-dark border-3 text-center ${vm.colorFn(Number(siteFirst.AQI))}">
+      <sapn class="display-6 py-4 fw-bolder">${siteFirst.AQI}</sapn>
+    </div>
+    <div class="col px-0">
+      <ul class="list-group list-group-flush border-top border-dark border-4 px-3">
+        <li class="list-group-item d-flex justify-content-between border-dark py-3"> 
+          <h4 class="d-inline fw-bolder mb-0">臭氧 <span>O3 (ppb)</span></h4> 
+          <span class="h4 fw-weight mb-0">${siteFirst.O3}</span></li>
+        <li class="list-group-item d-flex justify-content-between border-dark py-3"> 
+          <h4 class="d-inline fw-bolder mb-0">懸浮微粒 <span>PM10 (μg/m³)</span></h4> 
+          <span class="h4 fw-weight mb-0">${siteFirst.PM10}</span></li>
+        <li class="list-group-item d-flex justify-content-between border-dark py-3"> 
+          <h4 class="d-inline fw-bolder mb-0">細懸浮微粒 <span>PM2.5 (μg/m³)</span></h4> 
+          <span class="h4 fw-weight mb-0">${siteFirst["PM2.5"]}</span></li>
+        <li class="list-group-item d-flex justify-content-between border-dark py-3"> 
+          <h4 class="d-inline fw-bolder mb-0">一氧化碳 <span>CO (ppm)</span></h4> 
+          <span class="h4 fw-weight mb-0">${siteFirst.CO}</span></li>
+        <li class="list-group-item d-flex justify-content-between border-dark py-3"> 
+          <h4 class="d-inline fw-bolder mb-0">二氧化硫 <span>SO2 (ppb)</span></h4> 
+          <span class="h4 fw-weight mb-0">${siteFirst.SO2}</span></li>
+        <li class="list-group-item d-flex justify-content-between border-dark py-3"> 
+          <h4 class="d-inline fw-bolder mb-0">二氧化氮 <span>NO2 (ppb)</span></h4> 
+          <span class="h4 fw-weight mb-0">${siteFirst.NO2}</span></li>
+      </ul>
+    </div>`
     sitesAry.forEach( item => {
-      console.log(item.SiteName + item.AQI)
-      sitesStr += `<div class="col-4">
+      sitesOtherStr += `
+      <div class="col-6">
         <div class="row justify-content-center align-items-center border border-dark border-4">
           <div class="col-7 text-center">
             <span class="h2 fw-bolder">${item.SiteName}</span>
@@ -66,13 +98,13 @@ const searchAir = function() {
         </div>
       </div>`
     })
-    sites.innerHTML = sitesStr
+    siteDetail.innerHTML = siteDetailStr
+    siteOther.innerHTML = sitesOtherStr
   }
   vm.getData()
 }
 
 const searchAirFn = new searchAir()
-
 
 // 監聽選單
 areasOptions.addEventListener('change', searchAirFn.changeFn)
