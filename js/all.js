19f5c7e46a976c92a83c0edd38f5bd18f376a1dc
Vic <Vic@gmail.com>
2021-01-13 17:09:53 +0800

    新增 點擊站點顯示資料

--- js/all.js
+++ js/all.js
@@ -1,14 +1,18 @@
 const googleScript = `https://script.googleusercontent.com/macros/echo?user_content_key=phrVdYu0Q3XqAxTa4cq72gaVj8KRw8fwK8GkMW8L9IF_WGg8NmQCIVDWpRYBpfh-fXZ7nRVcoyTFky8F32I5fLKI9_cAMlzPm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIoD3KqIE4zKFslomcrc1HsTxzETLkHyqKpbVKR7SUZ7yF9sZFB2hWkNIdwvNj24Yg3NRYZ4K8s_vJsw0wqpxiI&lib=MsigB4MHVny8Dzc9UZJu-ZrYZRSJ0wUZU`
 const areasOptions = document.querySelector('.js-areas')
+const siteOther = document.querySelector('.js-siteOther')
 
 const searchAir = function() {
   const area =  document.querySelector('.js-area')
   const updateTime = document.querySelector('.js-updateTime')
   const siteDetail = document.querySelector('.js-siteDetail')
-  const siteOther = document.querySelector('.js-siteOther')
+  // 原始資料
   let date = []
+  // 區域資料
   let areas = []
   let areasStr = '<option selected>-請選擇地區-</option>'
+  // 站點資料
+  let sitesAry = []
   let vm = this
   this.getData = () => {
     fetch( googleScript )
@@ -17,18 +21,15 @@ const searchAir = function() {
     })
     .then( jsonData => {
       date = jsonData
-      vm.render(date)
+      date.forEach( item => {
+        if ( areas.indexOf(item.County) === -1 ) {
+          areas.push( item.County ) 
+          areasStr += `<option value="${item.County}">${item.County}</option>`
+          updateTime.innerHTML = `${item.PublishTime} 更新`
+        }
+      })
+      areasOptions.innerHTML = areasStr
     })
-  }
-  this.render = ( openData ) => {
-    openData.forEach( item => {
-      if ( areas.indexOf(item.County) === -1 ) {
-        areas.push( item.County ) 
-        areasStr += `<option value="${item.County}">${item.County}</option>`
-        updateTime.innerHTML = `${item.PublishTime} 更新`
-      }
-    })
-    areasOptions.innerHTML = areasStr
   }
   this.colorFn = (num) => {
     if ( num >=0 && num <=50 ) {
@@ -45,17 +46,11 @@ const searchAir = function() {
       return 'bg-peril'
     } 
   } 
-  this.changeFn = (e) => {
-    let sitesAry = []
+  this.render = (areaName) => {
     let siteDetailStr = ''
     let sitesOtherStr = ''
-    area.textContent = e.target.value
-    date.forEach(item => {
-      if ( item.County === e.target.value ) {
-        sitesAry.push(item)
-      }
-    })
     let siteFirst = sitesAry.shift()
+    area.textContent = areaName
     siteDetailStr = `
     <div class="col-7 text-center">
       <span class="h2 fw-bolder">${siteFirst.SiteName}</span>
@@ -88,23 +83,40 @@ const searchAir = function() {
     sitesAry.forEach( item => {
       sitesOtherStr += `
       <div class="col-6">
-        <div class="row justify-content-center align-items-center border border-dark border-4">
+        <a href="#" class="js-siteLink row justify-content-center align-items-center border border-dark border-4 text-decoration-none text-reset">
           <div class="col-7 text-center">
             <span class="h2 fw-bolder">${item.SiteName}</span>
           </div>
           <div class="col-5 py-4 border-start border-dark border-3 text-center ${vm.colorFn(Number(item.AQI))}">
             <sapn class="display-6 py-4 fw-bolder">${item.AQI}</sapn>
           </div>
-        </div>
+        </a>
       </div>`
     })
+    sitesAry.push(siteFirst)
     siteDetail.innerHTML = siteDetailStr
     siteOther.innerHTML = sitesOtherStr
   }
+
+  this.changeFn = (e) => {
+    sitesAry = []
+    date.forEach(item => {
+      if ( item.County === e.target.value ) {
+        sitesAry.push(item)
+      }
+    })
+    vm.render(e.target.value)
+  }
+  // this.clickSite = () => {
+    
+  // }
+  // this.addEventFn = (e) => {
+    
+  // }
   vm.getData()
 }
-
 const searchAirFn = new searchAir()
 
 // 監聽選單
 areasOptions.addEventListener('change', searchAirFn.changeFn)
+siteOther.addEventListener('click', searchAirFn.addEventFn)
